78ae2ccc2c2d829e16437850e7af477054d24acc
MellongLau <121382647@qq.com>
2015-11-20 17:26:58 +0800

    added url.

--- AMAnimatedNumber/AMAnimatedNumber.h
+++ AMAnimatedNumber/AMAnimatedNumber.h
@@ -2,6 +2,7 @@
 //  AMAnimatedNumber.h
 //
 //  Copyright (c) 2015 Mellong Lau
+//  https://github.com/MellongLau/AMAnimatedNumber
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
