969d93052fd2a415ad37ac86180a3b89239565d8
MellongLau <121382647@qq.com>
2015-11-20 17:39:10 +0800

    added demo project

---
+++ Demo/AMAnimatedNumberDemo/ViewController.m
@@ -0,0 +1,70 @@
+//
+//  ViewController.m
+//
+//  Copyright (c) 2015 Mellong Lau
+//  https://github.com/MellongLau/AMAnimatedNumber
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+#import "ViewController.h"
+#import "AMAnimatedNumber.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet AMAnimatedNumber *animateNumber;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UISwitch *swtich;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onSetNumberButtonClicked:(UIButton *)sender {
+    [self.view endEditing:YES];
+    [_animateNumber setTextFont:[UIFont boldSystemFontOfSize:26]];
+    [_animateNumber setTextColor:[UIColor brownColor]];
+    
+    [_animateNumber setNumbers:_textField.text
+                      animated:_swtich.on
+                     direction:_segmentedControl.selectedSegmentIndex == 0 ? AMAnimateNumberDirectionUp:AMAnimateNumberDirectionDown];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onSwitchChanged:(UISwitch *)sender {
+    [self.view endEditing:YES];
+}
+
+@end
---
+++ Demo/AMAnimatedNumberDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AMAnimatedNumberDemo
+//
+//  Created by Johnny Liu on 15/11/20.
+//  Copyright © 2015年 tendencystudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
